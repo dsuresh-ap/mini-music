32cc1b477f740f421c702bf10466add57baf74f9
dsuresh-ap <dhananjay.suresh1@gmail.com>
2025-10-05 21:17:41 -0400

    Init app

---
+++ Mini Music/Mini MusicTests/Mini_MusicTests.swift
@@ -0,0 +1,16 @@
+//
+//  Mini_MusicTests.swift
+//  Mini MusicTests
+//
+//  Created by Dhananjay Suresh on 10/5/25.
+//
+
+import Testing
+
+struct Mini_MusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
